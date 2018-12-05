a8bea40be33c754772f5097431aca350346f4589
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2018-12-05 19:14:52 +0200

    test

--- src/redux/actions/initial.store.actions.js
+++ src/redux/actions/initial.store.actions.js
@@ -1,7 +1,13 @@
 import countriesData from '../../data/dataCountries';
 import citiesData from '../../data/dataCities';
 
-export const initinalCountriesAction = () => dispatch => {
+export const initinalCountriesAction = () => dispatch =>{
+  // return dispatch => {
+  //   fetch('http://localhost:3000/countries/all')
+  //     .then(data => data.json())
+  //     .then(data => dispatch({type: 'INIT_COUNTRIES', payload: data}))
+  //     .catch(err => console.error(err));
+  
   return dispatch({type: 'INIT_COUNTRIES', payload: countriesData})
 }
 
